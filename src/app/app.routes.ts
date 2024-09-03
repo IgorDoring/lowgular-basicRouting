fac4a9293c59325efb389f6566e1d47a113c0cbe
IgorDoring <idoring@tutanota.com>
2024-09-03 13:29:15 -0300

    feat: creating and starting list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "list",
+        pathMatch: 'full'
+    },
+    {
+        path: "list",
+        title: "Users List",
+        component: ListComponent
+    }
+];
